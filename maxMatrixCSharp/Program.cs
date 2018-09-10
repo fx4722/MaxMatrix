06526337bcff321cc34ca2730a0e9cf90cc687e9
fx4722 <jessica_fx@163.com>
2018-09-10 15:21:46 +0800

    Add files via upload

---
+++ maxMatrixCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxMatrixCSharp
+{
+    class Program
+    {
+        [DllImport(@"maxMatixDll.dll", EntryPoint = "ex_maxmatrix", SetLastError = true, CharSet = CharSet.Ansi,
+ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        static extern double ex_maxMatrix(double[] a, int row, int col);
+
+        static void Main(string[] args)
+        {
+            double[] a = new double[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            double b;
+            b = ex_maxMatrix(a, 3, 3);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ maxMatrixWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
+
+namespace maxMatrixWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport(@"maxMatixDll.dll", EntryPoint = "ex_maxmatrix", SetLastError = true, CharSet = CharSet.Ansi,
+ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        static extern double ex_maxMatrix(double[] a, int row, int col);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double[] a = new double[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            double b;
+            b = ex_maxMatrix(a, 3, 3);
+            textbox1.Text = b.ToString();
+        }
+    }
+}
